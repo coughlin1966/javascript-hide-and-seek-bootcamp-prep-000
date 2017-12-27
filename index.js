81bd29be8c7a25196a47bd1d159d18f1eeb16af9
Peter Coughlin <coughlin1966@gmail.com>
2017-12-27 17:35:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,44 +15,13 @@ function increaseRankBy(n) {
   }
 }
 
-/*
 function deepestChild() {
-  const criteriaFn = 'boo'
-  let array = document.getElementById('app').querySelectorAll('#grand-node')
-  let current = array
-  let next = []
-  while (current) {
-    if (current = criteriaFn) {
-      return current
-    }
-    if (array.isArray(current)) {
-      for (let i = 0; i < current.length; i++) {
-        next.push(current[i])
-  }
-}
-current = next.shift()
-  }
-  return null
-}
-
-function deepestChild() {
-  //var grandNode = document.getElementById('app').querySelectorAll('div#grand-node')
-  var grandNode = document.querySelector('#grand-node');
-  var deepestNode = grandNode.children[0];
-  for (var i = 0; !deepestNode.children[i]; i++) {
-    deepestNode = deepestNode.children[i]
-  }
-  return deepestNode.innerHTML;
-}
-*/
-
-function deepestChild() {
-  let grand = document.querySelector('div#grand-node')
-  let next = grand.children[0]
+  let grand = document.querySelector('div#grand-node');
+  let next = grand.children[0];
 
   while (next) {
-    grand = next
-    next = grand.children[0]
+    grand = next;
+    next = grand.children[0];
   }
-return grand
+return grand;
 }

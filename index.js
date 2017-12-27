f9bd17e1185f770eca6de8c8ef94421d35e6cb29
Peter Coughlin <coughlin1966@gmail.com>
2017-12-27 17:29:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -48,11 +48,11 @@ function deepestChild() {
 
 function deepestChild(){
   let grand = document.querySelector('div#grand-node')
-  let next = grand.child[0]
+  let next = grand.children[0]
 
   while (next) {
     grand = next
-    next = grand.child[0]
+    next = grand.children[0]
   }
 return grand
 }

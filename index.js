afe1c318708347668b90dab5a6b357135664e80e
Peter Coughlin <coughlin1966@gmail.com>
2017-12-27 17:03:19 +0000

    Done.

--- index.js
+++ index.js
@@ -7,3 +7,30 @@ function nestedTarget() {
 var nestedElement = document.getElementById('nested').querySelectorAll('div.target');
 return nested;
 }
+
+function increaseRankBy(n) {
+  var upRank = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+  for (let i = 0; i < upRank.length; i++) {
+  upRank[i].innerHTML = parseInt(upRank[i].innerHTML) + n
+  }
+}
+
+
+function deepestChild() {
+  const criteriaFn = 'boo'
+  let array = document.getElementById('app').querySelectorAll('div#grand-node')
+  let current = array
+  let next = []
+  while (current) {
+    if (current = criteriaFn) {
+      return current
+    }
+    if (array.isArray(current)) {
+      for (let i = 0; i < current.length; i++) {
+        next.push(current[i])
+  }
+}
+current = next.shift()
+  }
+  return null
+}
